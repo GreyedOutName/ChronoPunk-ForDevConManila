5eb43dbb09571b227e1cc2c03ecd5832d761bbed
GreyedOutName <132237772+GreyedOutName@users.noreply.github.com>
2025-08-09 22:57:54 +0800

    fixed bug on moving

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 100;
+var speed = 120;
 var click_position;
 var target_position = null;
 var Astar;
@@ -12,7 +12,7 @@ func _ready():
 func movePlayer(roughTargetPosition):
 	#next_point and current_point gives values equal to a point index in the Astar Object
 	var next_point = Astar.get_closest_point(roughTargetPosition)
-	var current_point = Astar.get_closest_point(position)
+	var current_point = Astar.get_closest_point(global_position)
 	if next_point!=current_point:
 		var path = Astar.get_point_path(current_point,next_point)
 		target_position = path[1]
@@ -22,10 +22,16 @@ func _physics_process(_delta):
 		click_position = get_global_mouse_position()
 		movePlayer(click_position)
 		
-	if target_position != global_position && target_position != null:
-		var direction = target_position - global_position
-		velocity = direction * speed
-		move_and_slide()
+	if target_position != null:
+		var distance = global_position.distance_to(target_position);
+		if distance > 4:
+			var direction = (target_position - global_position).normalized()
+			velocity = direction * speed
+		else:
+			global_position = target_position  # snap exactly to point
+			velocity = Vector2.ZERO
 	else:
 		velocity = Vector2.ZERO
+		
+	move_and_slide()
 		
