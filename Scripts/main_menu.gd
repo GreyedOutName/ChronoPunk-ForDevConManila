081be2615c12de38520c585f3933e744a43964bb
TheOGKtura <theogktura@gmail.com>
2025-08-14 07:18:55 +0800

    Added music and simple button functionalities

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
-
 func _on_play_button_button_down():
 	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
+	MusicPlayer.play_song("PB - Data Shortcut")
 
-# Implement options, credits, etc.
+# Implement credits and quit
+func _on_quit_button_button_down() -> void:
+	get_tree().quit()
