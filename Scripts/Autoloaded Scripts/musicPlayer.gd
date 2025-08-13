27a7f0408d6f1ffc8c6a9bbc8007662b877c424c
TheOGKtura <theogktura@gmail.com>
2025-08-14 07:21:07 +0800

    font change and music additions

---
+++ Scripts/Autoloaded Scripts/musicPlayer.gd
@@ -0,0 +1,33 @@
+extends AudioStreamPlayer
+
+@onready var dummy_player = AudioStreamPlayer.new()
+var fading = false
+
+func _ready() -> void:
+	add_child(dummy_player)
+	
+	stream = load("res://Assets/sounds/music/WBA - Empty City.wav")
+	self.volume_db = -30
+	play()
+
+func _process(delta: float) -> void:
+	if fading:
+		volume_db -= 30*delta
+		dummy_player.volume_db += 30*delta
+		
+		if dummy_player.volume_db >= -30:
+			volume_db = -30
+			dummy_player.volume_db = -60
+			
+			stream = dummy_player.stream
+			play(dummy_player.get_playback_position())
+			
+			dummy_player.stop()
+			fading = false
+
+func play_song(song_name) -> void:
+	dummy_player.stream = load("res://Assets/sounds/music/" + song_name + ".wav")
+	dummy_player.volume_db = -30
+	dummy_player.play()
+	
+	fading = true
